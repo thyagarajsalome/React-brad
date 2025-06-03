1b8e4d85f76e9e20b52f57ab462570db39e34f04
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-06-03 13:16:49 +0530

    feeback UI added

---
+++ feedback-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header text="Welcome to My App" />
+
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
