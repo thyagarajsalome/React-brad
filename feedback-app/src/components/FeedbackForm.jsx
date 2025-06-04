ba33f540886c96ff45bf767a9b289b68385ed450
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-06-04 08:58:32 +0530

    Button component added

---
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+
+const FeedbackForm = () => {
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us </h2>
+        {/* {@todo - rating select component} */}
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" version="primary">
+            Send
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default FeedbackForm;
